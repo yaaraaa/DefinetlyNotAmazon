1ddfbd9f2b43fb0286eda1ed182e604ea17c9167
@midoplays <M.TarekSaad@nu.edu.eg>
2022-12-30 06:59:51 +0200

    new frontend

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="home">
+    <section class="hero is-medium is-ligh mb-6">
+      <div class="hero-body has-text-centered">
+        <p class="title mb-6">
+            Welcome to HighTech
+        </p>
+        <p class="subtitle">
+            The best tech store online
+        </p>
+      </div>
+    </section>
+
+    <div class="column is-multiline">
+      <div class="column is-12">
+        <h2 class="is-size-2 has-text-centered"> Latest products</h2>
+      </div>
+    <!--<div 
+          class="column is-3"
+          v-for="product in latestProducts
+          v-bind:key="product.id"
+        >
+          <div class="box">
+            <figure class="image mb--4"
+              <img v-bind:src="product.get_thumbnail">
+            </figure>
+
+            <h3 class="is-size-4">{{ product.name }}</h3>
+            <p class="is-size-6 has-text-grey"> ${{ product.price }}</p>
+
+            View details (will be a button)
+          </div>
+        </div>
+        -->
+
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'HomeView',
+  data() {
+    return {
+      latestProducts: []
+    }
+  },
+  components: {
+    
+  },
+  mounted() {
+    this.getLatestProducts()
+  },
+  methods: {
+    getLatestProducts() {
+      axios
+        .get(/*something*/)
+        .then(response => {
+          this.latestProducts = response.data
+        })
+        .catch(error => {
+          console.log(error)
+        })
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .image {
+    margin-top: -1.25rem;
+    margin-left: -1.25rem;
+    margin-right: -1.25rem;
+  }
+</style>
---
+++ frontend/src/views/ProductView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="page-product">
+    <div class="column is-multiline">
+        <div class="column is-9">
+            <figure class="image mb-6">
+                <img v-bind:src="product.get_image">
+            </figure>
+
+            <h1 class="title">{{ product.name }}</h1>
+            <p>{{ product.descriptions }}</p>
+        </div>
+
+        <div class="column is-3">
+            <h2 class="subtitle">Information</h2>
+
+            <p><strong>Price: </strong>${{ product.price }}</p>
+            
+        </div>
+
+    </div>
+  </div>
+  
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    name: 'ProductView',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
