7e3b12a2a676c3aa3edb165faa6b18684bba6932
@midoplays <M.TarekSaad@nu.edu.eg>
2022-12-30 17:27:58 +0200

    Introduced data loader function

--- backend/app.py
+++ backend/app.py
@@ -1,29 +1,11 @@
 from flask import Flask, jsonify, request, render_template
 from flask_mysqldb import MySQL
 from flask_cors import CORS
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_marshmallow import Marshmallow
-
-# from io import BytesIO
-# from PIL import Image
 import ast
 
 
 app = Flask(__name__)
-
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:"DefinetlyNotAmazon"@localhost/onlShop'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# db = SQLAlchemy(app)
-# ma = Marshmallow(app)
 CORS(app)
-
-
-# class ProductSchema(ma.Schema):
-#     class Meta:
-#         fields = ('date_added', 'price', 'image', 'brand', 'model', 'quantity', 'name', 'discount_amount', 'date_updated')
-
-# product_schema = ProductSchema()
-# products_schema = ProductSchema(many=True)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
@@ -32,6 +14,7 @@ app.config['MYSQL_DB'] = 'onlShop'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 mysql = MySQL(app)
+
 
 @app.route('/register', methods = ['GET', 'POST'])
 def Register():
@@ -90,61 +73,39 @@ def login():
 
 @app.route('/home', methods=['GET'])
 def home():
+    return load_data('product')
     
-
-    # print(all_products)
-    # print(fields)
-    # r = [{'hello':'world'},{'fuck':'world'}]
-
-    # for i in r:
-        # yield r
-    return load_data()
-    
-    # print(jsonify(results))
-    # return jsonify(results)
-    # return jsonify({image:"https://images-na.ssl-images-amazon.com/images/I/71oVh2UO8xL._SL1500_.jpg,http://pisces.bbystatic.com/image2/BestBuy_US/images/products/5689/5689019_sa.jpg,https://images-na.ssl-images-amazon.com/images/I/81EzIaCamJL._SL1500_.jpg,https://images-na.ssl-images-amazon.com/images/I/71X2tAW39aL._SL1500_.jpg,https://images-na.ssl-images-amazon.com/images/I/71c8xybNthL._SL1500_.jpg,http://static.bhphoto.com/images/smallimages/1368452917000_911749.jpg,http://static.bhphoto.com/images/images500x500/sanus_vlf410_b1_vlf410_super_slim_full_motion_1368452917000_911749.jpg,http://static.bhphoto.com/images/multiple_images/thumbnails/1368452838000_IMG_316474.jpg"})
-
 
 def dictify(request):
     'Converts request byte object to dictionary'
     return ast.literal_eval(request.get_data().decode('UTF-8'))
 
 
-def load_data():
+def load_data(table):
     cursor = mysql.connection.cursor()
-    query_string = "SELECT * FROM product;"
+
+    match table:
+        case 'product':
+            query_string = "SELECT * FROM product;"
+
+        
+        case 'customer':
+            query_string = "SELECT * FROM customer"
+
+
     cursor.execute(query_string)
-    all_products = cursor.fetchall()
-    # results = products_schema.dump(all_products)
+    query = cursor.fetchall()
     cursor.close()
-    fields = (
-        'product_id',
-        'date_added',
-        'price',
-        'image',
-        'brand',
-        'model',
-        'quantity',
-        'name',
-        'discount_amount',
-        'date_updated'
-    )
-    a = {}
-    b = []
-    for idx,i in enumerate(all_products):
-        a.update(zip(fields, i))
-        b.append(a)
-    return jsonify(b)
-    
-    # for i,instance in enumerate(all_products):
-    #     for record in instance:
-    #         if record == 0:
-    #             print(fields)
-    #             return fields
-    #         for field in fields:
-    #         # print("shit: ",record[i])
-    #             # print(record)
-    #             field = record
+
+    fields = []
+    for items in cursor.description:
+        fields.append(items[0])
+
+    instance, all_instances = {}, []
+    for row in query:
+        instance.update(zip(fields, row))
+        all_instances.append(instance)
+    return jsonify(all_instances)
 
 
 if __name__ == "__main__":
