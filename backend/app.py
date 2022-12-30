208f72a65a601e8729c02fe94c49a1d81017357c
@midoplays <M.TarekSaad@nu.edu.eg>
2022-12-31 00:13:59 +0200

    Latest products

--- backend/app.py
+++ backend/app.py
@@ -72,7 +72,14 @@ def login():
 
 @app.route('/home', methods=['GET'])
 def home():
-    return load_data('product')
+    cursor = mysql.connection.cursor()
+    query_string = "SELECT * FROM product ORDER BY date_added DESC LIMIT 5;"
+    cursor.execute(query_string)
+    query = cursor.fetchall()
+    data = load_data(cursor, query)
+    cursor.close()
+
+    return data
     
 
 def dictify(request):
@@ -80,12 +87,12 @@ def dictify(request):
     return ast.literal_eval(request.get_data().decode('UTF-8'))
 
 
-def load_data(table):
-    cursor = mysql.connection.cursor()
-    query_string = "SELECT * FROM %s"% (table)
-    cursor.execute(query_string)
-    query = cursor.fetchall()
-    cursor.close()
+def load_data(cursor, query):
+    # cursor = mysql.connection.cursor()
+    # query_string = "SELECT * FROM %s"% (table)
+    # cursor.execute(query_string)
+    # query = cursor.fetchall()
+    # cursor.close()
 
     fields = []
     for items in cursor.description:
