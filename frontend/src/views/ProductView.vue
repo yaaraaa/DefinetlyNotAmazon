3b9277271271d238c4267fb592ec36c7d738dfe7
@midoplays <M.TarekSaad@nu.edu.eg>
2023-01-01 01:55:10 +0200

    p

--- frontend/src/views/ProductView.vue
+++ frontend/src/views/ProductView.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="page-product">
     <figure class="image mb-6">
-        <img v-bind:src="product.image">
+            <img v-bind:src="product.image">
     </figure>
+    
   </div>
+  
 </template>
 
 <script>
@@ -24,13 +26,19 @@ export default {
     methods: {
         getProduct() {
           
-          // get product code
+          axios.get('/product/:id')
+          .then(response => {
+            this.product = response.data
+            console.log(response.data)
+          })
+          .catch(error => {
+            console.log(error)
+          })
 
           // changing tab name
           document.title = this.product.name + ' | HighTech'
 
         },
-        
         addToCart() {
             // correct quantity value if any changes happen to it
             if (isNaN(this.quantity || this.quantity < 1)) {
